ab59f95be9e0afe80d8a6c0c48ad9e23380a86ca
sakthiveljoker <77488929+sakthiveljoker@users.noreply.github.com>
2021-08-18 17:15:04 +0530

    LargestPrime

---
+++ src/LargestPrime.java
@@ -0,0 +1,17 @@
+public class LargestPrime {
+    public static int getLargestPrime(int number) {
+      if(number <= 0 || number <= 1) {
+          return -1;
+      }
+      int largestPrime = 2;
+      while(largestPrime < number) {
+          if( number % largestPrime != 0) {
+              largestPrime++;
+          }else {
+              number = number / largestPrime;
+              //largestPrime = 2;
+          }
+      }
+      return number;
+    }
+}
